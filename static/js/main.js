022ba6e6fbca681367a8405da6f896d87b708974
Roman Kliuchkovych <kimasterr@gmail.com>
2015-06-19 20:55:02 +0300

    Designing UI/UX — step 1

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	doubleHover('a', 'hover');
+
+  $('.main_list-items-item-price a').priceFormat();
+
+  $('.fotorama').fotorama({
+    maxwidth: '100%',
+    nav: 'thumbs',
+    allowfullscreen: 'true'
+  });
+});
+
+
+// «doubleHover» by artpolikarpov
+var doubleHover = function(selector, hoverClass) {
+	$(document).on('mouseover mouseout', selector, function(e) {
+		$(selector)
+			.filter('[href="' + $(this).attr('href') + '"]')
+			.toggleClass(hoverClass, e.type == 'mouseover');
+	});
+
+}
