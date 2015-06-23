661f5eb3ff099a3aaa0898ab36c19ef372c09a3d
Budnyk <bezpoleznuj@gmail.com>
2015-06-23 07:50:51 +0300

    add trumbnails in admin

--- article/admin.py
+++ article/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from article.models import Article, Image, PlanHouse, PlanArea, Video
 from embed_video.admin import AdminVideoMixin
 from adminsortable2.admin import SortableAdminMixin
+
+
 # Register your models here.
 
 class ArticleInLine(admin.StackedInline):
@@ -25,12 +27,13 @@ class ArticleAdmin(SortableAdminMixin, admin.ModelAdmin):
                  (None, {'fields': ('article_short_title','article_text','article_outbuildings','article_service','article_fence',
                         'article_status','article_price','article_spase','article_rooms','article_floor','article_area','article_walls','article_foto')}),)
     #fields = ('article_short_title','article_text','article_date','article_status','article_price','article_spase','article_rooms','article_floor','article_area','article_walls','article_foto')
-    list_display = ['article_country','article_address','article_date', 'article_status']
+    list_display = ['article_thumbnail','article_country','article_address','article_date', 'article_status']
     list_display_links = ('article_country','article_address', 'article_date','article_status')
     inlines = [ArticleInLine, PlanHouseInLine, PlanAreaInLine, VideoInline]
     list_filter = ['article_date','article_status']
     ordering = ['article_order']
     search_fields = ['article_address']
+
 
 admin.site.register(Article, ArticleAdmin)
 
--- article/models.py
+++ article/models.py
@@ -3,7 +3,8 @@ from django.conf import settings
 from embed_video.fields import EmbedVideoField
 from shutil import copyfile
 from datetime import datetime
-import os
+import os,random,string
+
 # Create your models here.
 
 def resaize(filename,size=(1024, 768)):
@@ -18,42 +19,70 @@ class Article(models.Model):
         ordering = ["article_order"]
         verbose_name_plural = "Объявления"
     article_order = models.PositiveIntegerField(default=0, blank=False, null=False)
-    article_country = models.CharField('Страна',max_length=2, choices=(('HU', 'Венгрия'),('SK','Словакия')))
-    article_address = models.CharField('Адрес расположения',max_length=150,default='-')
-    article_short_title = models.TextField('Краткое описание объекта',max_length=300,default='-')
-    article_text = models.TextField('Полное описание объекта',default='-')
+    article_country = models.CharField('Страна',max_length=2, choices=(('HU', 'Венгрия'),('SK','Словакия')),default='HU')
+    article_address = models.CharField('Адрес расположения',max_length=150,blank=True)
+    article_short_title = models.TextField('Краткое описание объекта',max_length=300,blank=True)
+    article_text = models.TextField('Полное описание объекта',blank=True)
 
-    article_outbuildings = models.TextField('Дополнительные постройки и другие штуки на участке', default='-')
-    article_service = models.TextField('Стоимость коммунальных услуг и их перечень', default='-')
+    article_outbuildings = models.TextField('Дополнительные постройки и другие штуки на участке', blank=True)
+    article_service = models.TextField('Стоимость коммунальных услуг и их перечень', blank=True)
     article_fence = models.BooleanField('Огражденный Участок или нет',choices=((True, 'Да'),(False,'Нет')),default=True)
     article_date = models.DateTimeField('Дата Создания',default=datetime.now, blank=True )
-    article_status = models.BooleanField('Статус объекта',default=True)
-    article_price = models.CharField('Цена',max_length=10,default=0)
-    article_spase = models.IntegerField('Площадь',default=0)
-    article_rooms = models.IntegerField('Количество комнат',default=0)
-    article_floor = models.IntegerField('Количество этажей',default=0)
-    article_area = models.CharField('Наличие придомовой территории',max_length=50,default='-')
-    article_walls = models.CharField('Стены',max_length=50,default='Кирпич')
-    article_foto = models.ImageField('Фотография объекта', default='')
-    article_fotosmall = models.ImageField(default='')
-    article_fotomedium = models.ImageField(default='')
+    article_status = models.BooleanField('Статус объекта',default=True,blank=True)
+    article_price = models.CharField('Цена',max_length=15,default=0,blank=True)
+    article_spase = models.IntegerField('Площадь',default=0,blank=True)
+    article_rooms = models.IntegerField('Количество комнат',default=0,blank=True)
+    article_floor = models.IntegerField('Количество этажей',default=1,blank=True)
+    article_area = models.CharField('Наличие придомовой территории',max_length=50,blank=True)
+    article_walls = models.CharField('Стены',max_length=50,blank=True)
+    article_foto = models.ImageField('Фотография объекта', default='',blank=True)
+    article_fotosmall = models.ImageField(default='',blank=True)
+    article_fotomedium = models.ImageField(default='',blank=True)
     def save(self):
-        fileName, fileExtension = os.path.splitext(self.article_foto.name)
-        self.article_fotosmall =  fileName+'_s'+fileExtension
-        self.article_fotomedium =  fileName+'_m'+fileExtension
-        super(Article, self).save()
-        filename = os.path.join(settings.MEDIA_ROOT, self.article_foto.name)
-        resaize(filename,(1024,768))
+        if self.article_foto.name :
+            fileName, fileExtension = os.path.splitext(self.article_foto.name)
+            self.article_fotosmall =  fileName+'_s'+fileExtension
+            self.article_fotomedium =  fileName+'_m'+fileExtension
 
-        fileName, fileExtension = os.path.splitext(filename)
-        sname = fileName+'_s'+fileExtension
-        mname = fileName+'_m'+fileExtension
+            super(Article, self).save()
+            filename = os.path.join(settings.MEDIA_ROOT, self.article_foto.name)
+            resaize(filename,(1024,768))
 
-        copyfile(filename, sname)
-        resaize(sname,(160,106))
+            fileName, fileExtension = os.path.splitext(filename)
+            sname = fileName+'_s'+fileExtension
+            mname = fileName+'_m'+fileExtension
 
-        copyfile(filename,mname)
-        resaize(mname,(314,209))
+            copyfile(filename, sname)
+            resaize(sname,(160,106))
+
+            copyfile(filename,mname)
+            resaize(mname,(314,209))
+        else:
+            fileName, fileExtension = os.path.splitext('./null.JPG')
+            randomname = ''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(5))
+            nullname = fileName+randomname+fileExtension
+            nullnameway = os.path.join(settings.MEDIA_ROOT, './null.JPG')
+            copyfile(os.path.join(settings.MEDIA_ROOT, './null.JPG'),os.path.join(settings.MEDIA_ROOT, nullname))
+
+            fileName, fileExtension = os.path.splitext(nullname)
+            self.article_fotosmall=sname =  fileName+'_s'+fileExtension
+            self.article_fotomedium=mname =  fileName+'_m'+fileExtension
+
+            copyfile(nullnameway,os.path.join(settings.MEDIA_ROOT,sname))
+            resaize(os.path.join(settings.MEDIA_ROOT,sname),(160,106))
+
+            copyfile(nullnameway,os.path.join(settings.MEDIA_ROOT,mname))
+            resaize(os.path.join(settings.MEDIA_ROOT,mname),(314,209))
+
+            super(Article, self).save()
+
+    def article_thumbnail(self):
+        if self.article_fotosmall:
+            return u'<img src="%s" />' % self.article_fotosmall.url
+        else:
+            return '(Фотографии нет)'
+    article_thumbnail.short_description = 'Фотография'
+    article_thumbnail.allow_tags = True
 
 class Image(models.Model):
     class Meta():
